fc243757829b0bee81f8d6867d9d1e5adb625edc
Guadarrama <albertc.barron@gmail.com>
2020-01-14 16:11:48 -0500

    initial

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React, {useState} from 'react';
+
+const ToDoCard = props => {
+
+  const [name, setName] = useState("");
+  const [isComplete, setComplete] = useState(false);
+  const [items, setItems] = useState([
+    {name: "something", isComplete:true},
+    {name: "second task", isComplete:false},
+    {name: "third task here ---- aoetnhu", isComplete:false}
+  ]);
+
+  const addNinjaToList = e =>{
+    e.preventDefault();
+    let tempItems = [...items];
+    tempItems.push({name: name, isComplete:isComplete});
+    setItems(tempItems);
+    setName("");
+    setComplete(false);
+  }
+
+  const remove = i =>{
+    console.log(i);
+    let tempItems = [...items]
+    tempItems.splice(i, 1);
+    setItems(tempItems);
+  }
+
+  const toggleComplete = i =>{
+    console.log(i);
+    let tempItems = [...items];
+    tempItems[i].isComplete = !(items[i].isComplete);
+    setItems(tempItems);
+    console.log(items[i].isComplete);
+  }
+
+  return( 
+      <div className="container">
+        <h3 className="text-center">To Do List</h3>
+          {
+            items.map((item, i) =>
+              <div key={i} className="bg-light text-right">
+                <div className="d-inline-block">
+                  {(item.isComplete) ?
+                    <div><strike>{item.name} </strike></div>
+                    :
+                    <div>{item.name}</div>
+                  }
+                </div>
+                <div className="d-inline-block p-2">
+                  {(item.isComplete) ?
+                    <input
+                      type="checkbox" 
+                      onClick={ e => toggleComplete(i)}
+                      checked
+                    />                  
+                    :
+                    <input
+                      type="checkbox" 
+                      onClick={ e => toggleComplete(i)}
+                    />                    
+                  }
+                  &nbsp;
+
+                  <button onClick={ e => remove(i)} className="btn btn-outline-danger">Delete</button>
+                </div>
+              </div>
+            )
+          }  
+
+      <hr/>
+      <div>
+        <form onSubmit={ addNinjaToList } className="form-row justify-content-center">
+          <label className="col-form-label">To Do</label>
+          <input
+            type="text"
+            onChange={ e => setName(e.target.value)}
+            value={name} 
+          />
+          &nbsp;
+          <input type="submit" value="Add" className="btn btn-outline-info"/>
+        </form>
+      </div>
+
+    </div> 
+
+  );
+}
+
+function App() {
+  return (
+    <ToDoCard />
+  );
+}
+
+export default App;
+
