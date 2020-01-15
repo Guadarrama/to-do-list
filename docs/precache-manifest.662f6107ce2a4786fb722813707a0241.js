570d1eedeb44c7a1bd2f0c1cc447edf737152b01
Guadarrama <albertc.barron@gmail.com>
2020-01-14 19:02:09 -0500

    renamed build to docs

---
+++ docs/precache-manifest.662f6107ce2a4786fb722813707a0241.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7721ed57af2c9aa572c4a1a93f780887",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b808390044e7a731e5f2",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "998402fde1d196cb56bb",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b808390044e7a731e5f2",
+    "url": "/static/js/2.6356f343.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.6356f343.chunk.js.LICENSE"
+  },
+  {
+    "revision": "998402fde1d196cb56bb",
+    "url": "/static/js/main.03b8e45a.chunk.js"
+  },
+  {
+    "revision": "b4befee4902ee20241cb",
+    "url": "/static/js/runtime-main.c4b6f351.js"
+  }
+]);
