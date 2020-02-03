34eaf66f2080ab6946ed13acab0c3aa3c8bf1ec6
Guadarrama <albertc.barron@gmail.com>
2020-02-02 19:24:17 -0500

    Updates

---
+++ precache-manifest.c00064c54d9fba35bc28b2e628108e25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7273b0a4dd6519ddb94c84016ead17de",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "20ed3de088b5cf87484e",
+    "url": "/to-do-list/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "55268be3ac490cc430d5",
+    "url": "/to-do-list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "20ed3de088b5cf87484e",
+    "url": "/to-do-list/static/js/2.1bcf03a8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/to-do-list/static/js/2.1bcf03a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55268be3ac490cc430d5",
+    "url": "/to-do-list/static/js/main.72648790.chunk.js"
+  },
+  {
+    "revision": "ef1ab25b4db95c178911",
+    "url": "/to-do-list/static/js/runtime-main.7557c059.js"
+  }
+]);
